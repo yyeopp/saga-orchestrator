c8a0d42fbf99360d5c60a434ae547f0cce9a21a6
yyeopp <letsksy@naver.com>
2023-12-13 22:53:12 +0900

    Exception Constructor

--- src/main/java/shc/web/cluborche/exception/KafkaFailException.java
+++ src/main/java/shc/web/cluborche/exception/KafkaFailException.java
@@ -1,5 +1,22 @@
 package shc.web.cluborche.exception;
 
 public class KafkaFailException extends Exception {
+    public KafkaFailException() {
+    }
 
+    public KafkaFailException(String message) {
+        super(message);
+    }
+
+    public KafkaFailException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public KafkaFailException(Throwable cause) {
+        super(cause);
+    }
+
+    public KafkaFailException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
